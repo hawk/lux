c9a58e5de37937ce4f324ee9f632a599a0087d4f
Håkan Mattsson <hm@tail-f.com>
2015-01-26 17:31:18 +0100

    Copyright 2015
    git-svn-id: svn+ssh://repository.tail-f.com/svn/tailf-src/branches/confd-5.3@77325 2a660a1d-4e03-0410-a5f5-99798c2b1c65
    
    Conflicts:
    	system/test/lib/lux/Makefile
    	system/test/lib/lux/vsn.mk

--- c_src/runpty.c
+++ c_src/runpty.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2014 Tail-f Systems AB
+ * Copyright 2012-2015 Tail-f Systems AB
  *
  * See the file "LICENSE" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
