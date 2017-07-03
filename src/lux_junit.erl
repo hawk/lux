06e8e6631fa1f967ea7fffbadc66d12f629c5fdd
Mandus Elfving <melfving@cisco.com>
2017-07-03 09:13:56 +0200

    Use realtive path for JUnit classname
    Use a relative path for the classname instead of an absolute path to get
    the same classnames for multiple runs in different directories.

--- src/lux_junit.erl
+++ src/lux_junit.erl
@@ -6,49 +6,69 @@
 
 -module(lux_junit).
 
--export([write_report/2]).
+-export([write_report/3]).
 
 -include("lux.hrl").
 
-write_report(LogFile, _Opts) ->
+write_report(LogFile, RunDir, _Opts) ->
     File = lux_utils:normalize_filename(LogFile),
     case lux_log:parse_summary_log(File) of
         {ok, _Result, Groups, _ConfigSection, _FileInfo, _EventLogs} ->
-            Content = testsuites(Groups),
+            Content = testsuites(Groups, RunDir),
             Filename = File ++ ".junit.xml",
             file:write_file(Filename, Content);
         {error, _File, _Reason} = Error ->
             Error
     end.
 
-testsuites(Groups) ->
+testsuites(Groups, RunDir) ->
+    TestsuiteFun = fun(Group) ->
+                           testsuite(Group, RunDir)
+                   end,
     ["<?xml version=\"1.0\" encoding=\"utf-8\"?>",
      "<testsuites>",
-     lists:map(fun testsuite/1, Groups),
+     lists:flatmap(TestsuiteFun, Groups),
      "</testsuites>"].
 
-testsuite({test_group, _, Cases}) ->
+testsuite({test_group, _, Cases}, RunDir) ->
+    TestcaseFun = fun(Testcase) ->
+                          testcase(Testcase, RunDir)
+                  end,
     Tests = erlang:integer_to_binary(erlang:length(Cases)),
     ["<testsuite tests=\"", Tests, "\">",
-    lists:map(fun testcase/1, Cases),
-     "</testsuite>"].
+    lists:flatmap(TestcaseFun, Cases),
+     "</testsuite>"];
+testsuite(_, _) ->
+    [].
 
-testcase({test_case, Filename, _, _, _, Result}) ->
+testcase({result_case, Filename, Reason, _Details}, RunDir) ->
     ["<testcase ",
-     "classname=\"", classname(Filename), "\" ",
+     "classname=\"", classname(Filename, RunDir), "\" ",
+     "name=\"", name(Filename), "\">",
+     "<system-out>", Reason, "</system-out>",
+     "</testcase>"];
+testcase({test_case, Filename, _, _, _, Result}, RunDir) ->
+    ["<testcase ",
+     "classname=\"", classname(Filename, RunDir), "\" ",
      "name=\"", name(Filename), "\">",
      body(Result),
-     "</testcase>"].
+     "</testcase>"];
+testcase(_, _) ->
+    [].
 
-classname(Filename) ->
-    case string:join(filename:split(filename:dirname(Filename)), ".") of
-        [$/, $. | ClassName] -> ClassName;
-        ClassName            -> ClassName
+classname(Filename, RunDir) ->
+    DirName = lux_utils:drop_prefix(RunDir, filename:dirname(Filename)),
+    case string:join(filename:split(DirName), ".") of
+        "."                  -> "lux";
+        [$/, $. | ClassName] -> "lux." ++ ClassName;
+        ClassName            -> "lux." ++ ClassName
     end.
 
 name(Filename) ->
     filename:basename(Filename).
 
+body({result, skip}) ->
+    ["<skipped/>"];
 body({result, {_, LineNo, Expected, Actual, Details}}) ->
     ["<failure type=\"NoMatch\">",
      "<![CDATA[",
@@ -58,6 +78,19 @@ body({result, {_, LineNo, Expected, Actual, Details}}) ->
      "\nDetails:\n", join(Details, "\n"),
      "]]>",
      "</failure>"];
+body({error, Reason}) ->
+    ["<error message=\"error\" type=\"general_error\">",
+     "<![CDATA[",
+     join(Reason, "\n"),
+     "]]>",
+     "</error>"];
+body({error_line, LineNo, Reason}) ->
+    ["<error message=\"error\" type=\"general_error\">",
+     "<![CDATA[",
+     "Error at line ", LineNo,
+     join(Reason, "\n"),
+     "]]>",
+     "</error>"];
 body(_) ->
     [].
 
